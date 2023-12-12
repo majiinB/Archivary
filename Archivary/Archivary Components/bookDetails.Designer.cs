bf9805dc46ca5bc4e95bf76caa52c61a5923242b
ZerPoly <117883592+ZerPoly@users.noreply.github.com>
2023-12-12 19:23:38 +0800

    added bookDetails User Control

--- Archivary/Archivary Components/bookDetails.Designer.cs
+++ Archivary/Archivary Components/bookDetails.Designer.cs
@@ -28,18 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.LAYOUT_bookPicture = new System.Windows.Forms.Panel();
+            this.bookPicture = new System.Windows.Forms.PictureBox();
+            this.bookTitleLabel = new System.Windows.Forms.Label();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.isbnLabel = new System.Windows.Forms.Label();
+            this.viewButton = new roundedCorners.roundedButton();
+            this.LAYOUT_bookPicture.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bookPicture)).BeginInit();
             this.SuspendLayout();
+            // 
+            // LAYOUT_bookPicture
+            // 
+            this.LAYOUT_bookPicture.Controls.Add(this.bookPicture);
+            this.LAYOUT_bookPicture.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LAYOUT_bookPicture.Location = new System.Drawing.Point(0, 0);
+            this.LAYOUT_bookPicture.Name = "LAYOUT_bookPicture";
+            this.LAYOUT_bookPicture.Padding = new System.Windows.Forms.Padding(10);
+            this.LAYOUT_bookPicture.Size = new System.Drawing.Size(130, 200);
+            this.LAYOUT_bookPicture.TabIndex = 0;
+            // 
+            // bookPicture
+            // 
+            this.bookPicture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(190)))), ((int)(((byte)(143)))));
+            this.bookPicture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bookPicture.Location = new System.Drawing.Point(10, 10);
+            this.bookPicture.Name = "bookPicture";
+            this.bookPicture.Size = new System.Drawing.Size(110, 180);
+            this.bookPicture.TabIndex = 0;
+            this.bookPicture.TabStop = false;
+            // 
+            // bookTitleLabel
+            // 
+            this.bookTitleLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.bookTitleLabel.Font = new System.Drawing.Font("Montserrat Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bookTitleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(211)))), ((int)(((byte)(102)))));
+            this.bookTitleLabel.Location = new System.Drawing.Point(136, 10);
+            this.bookTitleLabel.Name = "bookTitleLabel";
+            this.bookTitleLabel.Size = new System.Drawing.Size(255, 55);
+            this.bookTitleLabel.TabIndex = 1;
+            this.bookTitleLabel.Text = "THE LORD OF THE RINGS\r\nTHE LORD OF THE RINGS\r\n";
+            this.bookTitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.authorLabel.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.authorLabel.Location = new System.Drawing.Point(136, 78);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(255, 23);
+            this.authorLabel.TabIndex = 2;
+            this.authorLabel.Text = "Author: <LASTNAME>, <FN> <MI>";
+            this.authorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // isbnLabel
+            // 
+            this.isbnLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.isbnLabel.Font = new System.Drawing.Font("Montserrat", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.isbnLabel.Location = new System.Drawing.Point(137, 101);
+            this.isbnLabel.Name = "isbnLabel";
+            this.isbnLabel.Size = new System.Drawing.Size(255, 29);
+            this.isbnLabel.TabIndex = 3;
+            this.isbnLabel.Text = "ISBN: 1436969143";
+            this.isbnLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // viewButton
+            // 
+            this.viewButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.viewButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(211)))), ((int)(((byte)(102)))));
+            this.viewButton.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(211)))), ((int)(((byte)(102)))));
+            this.viewButton.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.viewButton.BorderRadius = 40;
+            this.viewButton.BorderSize = 0;
+            this.viewButton.FlatAppearance.BorderSize = 0;
+            this.viewButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.viewButton.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewButton.ForeColor = System.Drawing.Color.White;
+            this.viewButton.Location = new System.Drawing.Point(184, 152);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(150, 40);
+            this.viewButton.TabIndex = 4;
+            this.viewButton.Text = "View Book";
+            this.viewButton.TextColor = System.Drawing.Color.White;
+            this.viewButton.UseVisualStyleBackColor = false;
             // 
             // bookDetails
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.viewButton);
+            this.Controls.Add(this.isbnLabel);
+            this.Controls.Add(this.authorLabel);
+            this.Controls.Add(this.bookTitleLabel);
+            this.Controls.Add(this.LAYOUT_bookPicture);
             this.Name = "bookDetails";
             this.Size = new System.Drawing.Size(400, 200);
+            this.LAYOUT_bookPicture.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bookPicture)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel LAYOUT_bookPicture;
+        private System.Windows.Forms.PictureBox bookPicture;
+        private System.Windows.Forms.Label bookTitleLabel;
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.Label isbnLabel;
+        private roundedCorners.roundedButton viewButton;
     }
 }
--- Archivary/Archivary Components/bookDetails.cs
+++ Archivary/Archivary Components/bookDetails.cs
@@ -16,5 +16,53 @@ namespace Archivary.Archivary_Components
         {
             InitializeComponent();
         }
+
+        #region Properties
+
+        private string _title;
+        private string _author;
+        private string _isbn;
+        private Image _image;
+        private Button _button;
+
+        [Category("Book Information")]
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; bookTitleLabel.Text = value; }
+        }
+
+        [Category("Book Information")]
+        public string Author
+        {
+            get { return _author; }
+            set { _author = value; authorLabel.Text = value; }
+        }
+
+        [Category("Book Information")]
+        public string ISBN
+        {
+            get { return _isbn; }
+            set { _isbn = value; isbnLabel.Text = value; }
+        }
+
+        [Category("Book Information")]
+        public Image Image
+        {
+            get { return _image; }
+            set { _image = value; bookPicture.Image = value; }
+        }
+
+        [Category("Book Information")]
+        public Button Button
+        {
+            get { return _button; }
+            set { _button = value; }
+        }
+        
+
+        #endregion
+
+
     }
 }
