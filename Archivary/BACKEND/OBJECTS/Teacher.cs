4891cdd6321b6bbe675b099f95f2474c21ccbfcd
Arthur Artugue <artuguearthur@gmail.com>
2023-12-21 14:12:04 +0800

    added backend folder containing, book and user operations as well as objects

---
+++ Archivary/BACKEND/OBJECTS/Teacher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivary.BACKEND.OBJECTS
+{
+    public class Teacher
+    {
+        public string TeacherId { get; set; }
+        public int TeacherUserId { get; set; }
+        public string TeacherFirstName { get; set; }
+        public string TeacherLastName { get; set; }
+        public string TeacherMiddleName { get; set; }
+        public string TeacherEmail { get; set; }
+        public string TeacherAddress { get; set; }
+        public string TeacherContactNum { get; set; }
+        public string TeacherDepartment { get; set; }
+        public string TeacherImagePath { get; set; }
+        public string TeacherStatus { get; set; }
+        public Teacher(string teacherId, int teacherUserId, string teacherFirstName, string teacherLastName, string teacherMiddleName,
+            string teacherEmail, string teacherAddress, string teacherContactNum, string teacherDepartment, string teacherImagePath, string teacherStatus)
+        {
+            TeacherId = teacherId;
+            TeacherUserId = teacherUserId;
+            TeacherFirstName = teacherFirstName;
+            TeacherLastName = teacherLastName;
+            TeacherMiddleName = teacherMiddleName;
+            TeacherEmail = teacherEmail;
+            TeacherAddress = teacherAddress;
+            TeacherContactNum = teacherContactNum;
+            TeacherDepartment = teacherDepartment;
+            TeacherImagePath = teacherImagePath;
+            TeacherStatus = teacherStatus;
+        }
+    }
+}
