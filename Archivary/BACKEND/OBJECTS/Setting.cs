b42da932080a1debe68728ecf170864824296b59
Arthur Artugue <artuguearthur@gmail.com>
2023-12-24 11:27:47 +0800

    added settings in db and set the attributes of setting objects

--- Archivary/BACKEND/OBJECTS/Setting.cs
+++ Archivary/BACKEND/OBJECTS/Setting.cs
@@ -8,5 +8,17 @@ namespace Archivary.BACKEND.OBJECTS
 {
     internal class Setting
     {
+        private int settingId;
+        private int borrowingCapacity;
+        private int reserveLimit;
+        private int borrowingDuration;
+        public Setting(int settingId, int borrowingCapacity, int reserveLimit, int borrowingDuration) 
+        {
+            this.settingId = settingId;
+            this.borrowingCapacity = borrowingCapacity;
+            this.reserveLimit = reserveLimit;
+            this.borrowingDuration = borrowingDuration;
+        }
+
     }
 }
